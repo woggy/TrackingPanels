a23a0b5c9acc238844364f02f66f37881ab39a13
woggy <woggy222@gmail.com>
2017-03-18 21:15:12 -0600

    Fixing nighttime tracking
    Reworked math for panel angles during the night. Will now wait until
    sunset, then (relatively) quickly reset to sunrise positions.

--- src/main/java/woggy/trackingpanels/BlockAxleTileEntity.java
+++ src/main/java/woggy/trackingpanels/BlockAxleTileEntity.java
@@ -16,8 +16,12 @@ public class BlockAxleTileEntity extends TileEntity
 		}
 		delay=20;
 		angle = this.getWorldObj().getCelestialAngle(0)*-360;
-		if(angle<-90)
-			angle=180-angle;
+		if(angle < -90 && angle >= -105)
+			angle = -90;
+		if(angle < -135 && angle > -270)
+			angle = -270;
+		if(angle < -105 && angle > -135)
+			angle = -6*(angle+105) - 90;
 	}
 
 }
--- src/main/java/woggy/trackingpanels/BlockSmallPanelTileEntity.java
+++ src/main/java/woggy/trackingpanels/BlockSmallPanelTileEntity.java
@@ -19,8 +19,12 @@ public class BlockSmallPanelTileEntity extends TileEntity
 		}
 		delay=20;
 		angle = this.getWorldObj().getCelestialAngle(0)*-360;
-		if(angle<-90)
-			angle=180-angle;
+		if(angle < -90 && angle >= -105)
+			angle = -90;
+		if(angle < -135 && angle > -270)
+			angle = -270;
+		if(angle < -105 && angle > -135)
+			angle = -6*(angle+105) - 90;
 	}
 	
 	@SideOnly(Side.CLIENT)
